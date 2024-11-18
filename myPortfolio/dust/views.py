bc531ebdeadac7c274d5f0c7e2ccb809bc13ab19
andyochieng <andyochieng18@gmail.com>
2024-11-18 17:11:10 +0300

    finished my project

--- myPortfolio/dust/views.py
+++ myPortfolio/dust/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+# dust/views.py
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 def contact(request):
-    return render(request, 'dust/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the contact form data to the database
+            return render(request, 'dust/contact.html', {'form': ContactForm(), 'success': True})
+    else:
+        form = ContactForm()
+    return render(request, 'dust/contact.html', {'form': form})
 
 def about(request):
     return render(request, 'dust/about.html')
 
 def projects(request):
     return render(request, 'dust/projects.html')
-def home (request):
+
+def home(request):
     return render(request, 'dust/home.html')
